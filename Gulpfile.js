0b732fda177573f3b22334c30971551c62f06ef9
ViniSilv27 <Vinicius.sv27@gmail.com>
2025-01-16 14:15:42 -0300

    finalizando o projeto

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCss = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'))
+}
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -15,7 +22,8 @@ function images(){
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles , images)
+exports.default = gulp.parallel(styles , images, scripts)
 exports.watch = function(){
-    gulp.watch('src/styles/*.scss', gulp.series(styles))
+    gulp.watch('src/styles/*.scss', gulp.series(styles));
+    gulp.watch('src/scripts/*.js', gulp.series(scripts))
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions =document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight
 
+    window.addEventListener('scroll', function(){
+        const alturaAtual = this.window.scrollY;
+
+        if(alturaAtual < alturaHero){
+            ocultaElementoHeader();
+
+        }
+
+        else{
+            apareceElementoHeader();
+        }
+    })
+
+
+
+    //sessão de shows
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             buttons[i].classList.remove('shows__tabs__button--is-active');
@@ -12,7 +31,31 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     } 
+
+
+    // sessão de faq
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abrirFecharAba);
+    }
 })
+
+function abrirFecharAba(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+function ocultaElementoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hadden')
+
+}
+ function apareceElementoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hadden')
+ }
+
 
 function escondeTodosbotoes(){
     const buttons = document.querySelectorAll('[data-tab-button]');
